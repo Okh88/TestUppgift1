5a92edafd0717f5bcb450721b872dcaa9e637d0b
Okh88 <orwah.khadrow@gmail.com>
2024-11-15 17:37:20 +0100

    Initial Commit

---
+++ TestUppgift1/TestUppgift1App.swift
@@ -0,0 +1,17 @@
+//
+//  TestUppgift1App.swift
+//  TestUppgift1
+//
+//  Created by Malmö Yrkehögskolan on 2024-11-15.
+//
+
+import SwiftUI
+
+@main
+struct TestUppgift1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
